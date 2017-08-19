85be1ecb4aebe287c58c64382c7e8d8b893bd5fe
bigazzzz <bigazzzz@gmail.com>
2017-08-20 02:52:30 +0300

    atom testing

--- App.php
+++ App.php
@@ -1,24 +1,23 @@
-<?php 
+<?php
 
 use App\Config;
 use App\Traits\Singleton;
 
-class App 
+class App
 {
 	use Singleton;
 
 	public function run()
 	{
 		try {
-			$this->route = \App\Route::get($_SERVER['REQUEST_URI']);
+			$this->route       = \App\Route::get($_SERVER['REQUEST_URI']);
 		} catch (\App\Exceptions\RouteException $e){
 			echo "Ошибка роутинга - " . $e->getMessage();
 		}
-		$controller = new $this->route->controller;
-		$this->session = \App\Session::instance();
+		$controller         = new $this->route->controller;
+		$this->session      = \App\Session::instance();
         $this->config = \App\Config::instance();
         $this->server = $_SERVER;
-
 		$controller->action($this->route->action, $this);
 	}
 }
