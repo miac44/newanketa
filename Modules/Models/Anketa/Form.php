0e11c4726921d56473be7011d83e55b5f0e96b66
bigazzzz <bigazzzz@gmail.com>
2017-08-23 01:20:23 +0300

    ValueTable added. First step

--- Modules/Models/Anketa/Form.php
+++ Modules/Models/Anketa/Form.php
@@ -11,7 +11,7 @@ class Form extends Model
     const COLUMNS = [
         'name' => ['type' => 'string'],
         'description' => ['type' => 'text'],
-        'action' => ['type' => 'string'],
+        'alias' => ['type' => 'string'],
     ];
     const RELATIONS = [
         'medicalOrganization' => ['type' => 'manyToMany', 'model' => '\Modules\Models\Anketa\MedicalOrganization'],
---
+++ Modules/Models/Anketa/ValueTable.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modules\Models\Anketa;
+
+use App\Model;
+
+class ValueTable extends Model
+{
+
+    const COLUMNS = [
+    ];
+
+    public function __construct($table_alias)
+    {
+        define('TABLE' , 'valuetable_' . $table_alias);
+    }
+
+}
