712b2096985536d96761f725493f0fc7160ee6f6
bigazzzz <bigazzzz@gmail.com>
2017-08-29 00:01:19 +0300

    DynamicModel added

--- Modules/Models/Anketa/DynamicModel.php
+++ Modules/Models/Anketa/DynamicModel.php
@@ -7,4 +7,37 @@ use App\Model;
 class DynamicModel extends Model
 {
 
+    public function __get($k)
+    {
+        $res = parent::__get($k);
+        if (!is_null($res)) {
+            return $res;
+        }
+
+        $method = 'get' . ucfirst($k);
+        if (method_exists($this, $method)) {
+            return $this->$method();
+        }
+
+        return null;
+    }
+
+    public function __isset($k)
+    {
+        $res = parent::__isset($k);
+        if (true === $res) {
+            return $res;
+        }
+
+        $method = 'get' . ucfirst($k);
+        if (method_exists($this, $method)) {
+            return $this->$method();
+        }
+    }
+
+    public function getMedicalOrganizationValues($k)
+    {
+        return $k;
+    }
+
 }
